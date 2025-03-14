f8e93fcda7d9391a0547765ed2631f8c83f8eeac
martinsinnona <martinsinnona@outlook.es>
2025-03-14 18:52:16 -0300

    a

--- datasets/dataset_web_ghost/test/check_alpha.sh
+++
@@ -1,7 +0,0 @@
-for file in *.png; do
-    if identify -format '%[channels]' "$file" | grep -q 'a'; then
-        echo "$file has transparency."
-    else
-        echo "$file does not have transparency."
-    fi
-done
